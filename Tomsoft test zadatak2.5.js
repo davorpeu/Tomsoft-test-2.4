66f2c10cee2ae5927c8a7406f044be232744b1da
dadop <davorpeu@gmail.com>
2021-06-09 15:38:12 +0200

    Init

---
+++ Tomsoft test zadatak2.5.js
@@ -0,0 +1,12 @@
+var discount_price;
+var discount;
+var price;
+var discount_calc;
+  
+price = <frdbProducts."retail_price">                                          
+discount= 0.2;
+discount_calc = price * discount;                                                                
+discount_price = price - discount_calc;                                                 
+
+                        
+
